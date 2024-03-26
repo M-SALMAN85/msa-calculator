8c7d5b057f77f85a86fb063345bf7ad3d7820c45
M SALMAN85 <m.salmanansari85@gmail.com>
2024-03-27 01:56:49 +0500

    calculator push to github

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Please Inter First Number",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: "Please Inter Second Number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Please Select",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please Put Valid Number");
+}
